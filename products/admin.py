38a16a780c9bae37336f2d2b4de982659f04e78a
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-07-27 12:46:42 +0300

    use tabularInline for product-images

--- products/admin.py
+++ products/admin.py
@@ -3,7 +3,15 @@ from .models import Product, Category, Brand, Review, ProductImages
 
 # Register your models here.
 
-admin.site.register(Product)
+
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesInline]
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Review)
