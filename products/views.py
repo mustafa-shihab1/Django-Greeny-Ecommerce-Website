91947fe3961a2c0f2d020850d361b08737141aca
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-07-31 12:17:30 +0300

    get product images using get_context_data()

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, ProductImages, Review
 # Create your views here.
 
 
@@ -13,3 +13,10 @@ class ProductList(ListView):
 class ProductDetail(DetailView):
     model = Product
 
+# get product images 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["images"] = ProductImages.objects.filter(product=self.get_object())
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        return context
+    
