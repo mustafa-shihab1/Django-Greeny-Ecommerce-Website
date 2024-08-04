a9a38b5a8be60b5a857b88f03246951be3b1b5ba
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-04 10:29:59 +0300

    add category and brand to urls

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail, CategoryList, BrandList
 
 app_name = 'products'
 
 urlpatterns = [
     path('',ProductList.as_view(),name='product_list'),
+    path('category/',CategoryList.as_view(),name='category_list'),
+    path('brand/',BrandList.as_view(),name='brand_list'),
     path('<slug:slug>',ProductDetail.as_view(),name='product_detail'),
 ]
