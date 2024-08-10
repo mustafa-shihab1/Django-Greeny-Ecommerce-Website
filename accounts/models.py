f9620572607f808dc1e61634f2ba3698d3ea2e1f
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-10 10:54:11 +0300

    connect User with Profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from settings.models import Country, City
 from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='Profile', on_delete= models.CASCADE)
     image = models.ImageField(upload_to='profile/')
 
 
@@ -18,13 +20,13 @@ DATA_TYPE=(
 
 
 class UserPhoneNumber(models.Model):
-    user = ''
-    phone_number = models.CharField(max_length=10)
+    user = models.OneToOneField(User, related_name='UserPhone', on_delete= models.CASCADE)
+    phone_number = models.CharField(max_length=15)
     type = models.CharField(max_length=10, choices=DATA_TYPE)
 
 
 class UserAddress(models.Model):
-    user = ''
+    user = models.OneToOneField(User, related_name='UserAddress', on_delete= models.CASCADE)
     type = models.CharField(max_length=10, choices=DATA_TYPE)
     country = models.ForeignKey(_("Country"),Country, on_delete=models.SET_NULL, related_name='user_country', null= True)
     city = models.ForeignKey(_("City"),City, on_delete=models.SET_NULL, related_name='user_city', null= True)
