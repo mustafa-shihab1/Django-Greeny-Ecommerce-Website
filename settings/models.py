038a0fd382059d9a9374fb433698db573196a826
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-11 17:11:05 +0300

    add verbose_name_plural

--- settings/models.py
+++ settings/models.py
@@ -6,13 +6,20 @@ from django.utils.translation import gettext as _
 class Country(models.Model):
     name = models.CharField(_("Country"), max_length=50)
 
+    class Meta:
+        verbose_name_plural = 'Countries'
+
     def __str__(self):
         return self.name
     
 
+
 class City(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='country_city')
     name = models.CharField(_("City"), max_length=50)
+
+    class Meta:
+        verbose_name_plural = 'Cities'
 
     def __str__(self):
         return self.name
