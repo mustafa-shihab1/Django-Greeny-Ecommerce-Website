33cd9cb6da2b6cf7cc81b7afac69385c23078610
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-11 14:16:59 +0300

    create country, city models

--- settings/models.py
+++ settings/models.py
@@ -2,3 +2,17 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(_("Country"), max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='country_city')
+    name = models.CharField(_("City"), max_length=50)
+
+    def __str__(self):
+        return self.name
