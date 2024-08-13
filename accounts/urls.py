253a135bd7a405a48834fa8b68c2d9e1317b1158
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-13 18:31:55 +0300

    add user_activate to accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import signup
+from .views import signup, user_activate
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('<str:username>/activate', user_activate, name='user_activate'),
 ]
