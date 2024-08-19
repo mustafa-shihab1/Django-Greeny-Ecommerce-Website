6966af06ed3c81b691b33442a5a5c2ec41f54e0b
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-19 19:05:17 +0300

    add slug to brand

--- products/models.py
+++ products/models.py
@@ -42,6 +42,13 @@ class ProductImages(models.Model):
 class Brand(models.Model):
     name = models.CharField(_("Name"), max_length=50)
     image = models.ImageField(_("Image"), upload_to='brand/')
+    slug = models.SlugField(_("Slug"), null=True, blank= True)
+
+    # when create any new brand it will create/save a new slug for it 
+    def save(self, *args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Brand, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
