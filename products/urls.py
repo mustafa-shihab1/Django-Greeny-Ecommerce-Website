2b73d8fe3c45f00c2321082a8a61ece5e20fb3bd
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-08-27 07:49:59 +0300

    test Queryset performance via product_list

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProductList, ProductDetail, CategoryList, BrandList, BrandDetail
+from .views import ProductList, ProductDetail, CategoryList, BrandList, BrandDetail, product_list
 
 app_name = 'products'
 
 urlpatterns = [
     path('',ProductList.as_view(),name='product_list'),
+    path('test',product_list),
     path('<slug:slug>',ProductDetail.as_view(),name='product_detail'),
     path('category/',CategoryList.as_view(),name='category_list'),
     path('brand/',BrandList.as_view(),name='brand_list'),
