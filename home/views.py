cde44d17622a4661ba4e34fc22b3cb94d3ebf986
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-11 12:31:06 +0300

    edit home view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from products.models import Category, Product
+from django.db.models.aggregates import Count
 
 # Create your views here.
+
+def home(request):
+    categories = Category.objects.all().annotate(product_count=Count('product_category'))
+    featured_products = Product.objects.filter(flag = 'Feature').order_by('?')[:6]
+    return render(request,'home/home.html',{'categories':categories, 'featured_products':featured_products})
