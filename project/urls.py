4b4b6d2c5ddbe5d8cbb1b6f2923f047dbb1fd947
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-11 12:29:17 +0300

    add home to urls and settings

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('home.urls',namespace='home')),
     path('accounts/',include('accounts.urls',namespace='accounts')),
     path('products/',include('products.urls',namespace='products')),
     path('orders/',include('orders.urls',namespace='orders')),
