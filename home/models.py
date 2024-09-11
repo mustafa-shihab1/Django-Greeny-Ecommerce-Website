7fbed4c945bcf713802980aac747f22c56464c90
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-11 12:30:40 +0300

    create banner model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class Banner(models.Model):
+    title = models.CharField(_("Title"), max_length=100)
+    subtitle = models.CharField(_("Sub-title"), max_length=400)
+    image = models.ImageField(_("Image"), upload_to='banner')
+    active = models.BooleanField(_("Active"), default=False)
+
+    def __str__(self):
+        return self.title
