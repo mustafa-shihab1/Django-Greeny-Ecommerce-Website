cfd436501883a0c51487172f6a7a9306bd185239
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-13 10:56:01 +0300

    add brands and new-product to home

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Category, Product
+from products.models import Category, Product, Brand
 from django.db.models.aggregates import Count
 
 # Create your views here.
@@ -7,4 +7,11 @@ from django.db.models.aggregates import Count
 def home(request):
     categories = Category.objects.all().annotate(product_count=Count('product_category'))
     featured_products = Product.objects.filter(flag = 'Feature').order_by('?')[:6]
-    return render(request,'home/home.html',{'categories':categories, 'featured_products':featured_products})
+    new_products = Product.objects.filter(flag = 'New').order_by('?')[:12]
+    brands = Brand.objects.all().annotate(product_count=Count('product_brand')).order_by('?')[:12]
+    return render(request,'home/home.html',{
+        'categories':categories,
+        'featured_products':featured_products,
+        'new_products':new_products,
+        'brands':brands,
+        })
