b80bd4fc4b55f62092ae44a873a3225308d9daf3
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-13 19:21:36 +0300

    add home bammer

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from products.models import Category, Product, Brand, Review
 from django.db.models.aggregates import Count
+from .models import Banner
 
 # Create your views here.
 
@@ -10,10 +11,12 @@ def home(request):
     new_products = Product.objects.filter(flag = 'New').order_by('?')[:12]
     brands = Brand.objects.all().annotate(product_count=Count('product_brand')).order_by('?')[:12]
     reviews = Review.objects.filter(rate__gt=3)
+    banners = Banner.objects.filter(active=True)
     return render(request,'home/home.html',{
         'categories':categories,
         'featured_products':featured_products,
         'new_products':new_products,
         'brands':brands,
         'reviews':reviews,
+        'banners':banners,
         })
