51dad027689fff140545fe7efb8148a134a53a72
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-18 07:40:02 +0300

    add to cart

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Order, OrderDetail
+from .models import Order, OrderDetail, CartOrder, CartDetail
+from products.models import Product
 from django.contrib.auth.decorators import login_required
 
 
@@ -8,3 +9,22 @@ from django.contrib.auth.decorators import login_required
 def order_list(request):
     orders = Order.objects.filter(user=request.user)
     return render(request,'orders/order_list.html',{'orders':orders})
+
+
+@login_required
+def add_to_cart(request):
+    if request.method == 'POST':
+        product_id = request.POST['productid']
+        quantity = request.POST['quantity']
+
+        product = Product.objects.get(id=product_id)
+        cart = CartOrder.objects.get(user=request.user, order_status='Inprogress')
+        cart_detail, created = CartDetail.objects.get_or_create(
+            order = cart,
+            product = product
+        )
+        cart_detail.quantity = int(quantity)
+        cart_detail.price = product.price
+        cart_detail.total = int(quantity) * product.price
+        cart_detail.save()
+        
