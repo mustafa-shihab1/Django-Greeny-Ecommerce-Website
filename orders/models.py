a00b0828a7122bc899b39bafbb72bc21994c4a3f
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-09-18 07:41:27 +0300

    get_total of cart items

--- orders/models.py
+++ orders/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 from products.models import Product
 from utils.generate_code import generate_code
 from django.contrib.auth.models import User
+from django.db.models.aggregates import Sum
 
 # Create your models here.
 
@@ -17,17 +18,25 @@ class CartOrder(models.Model):
     user = models.ForeignKey(User, verbose_name=_("User"),related_name='user_cart', on_delete=models.SET_NULL, null=True, blank=True)
     code = models.CharField(_("Code"), default=generate_code, max_length=8,)
     order_status = models.CharField(_("Order Status"), max_length=10, choices=STATUS_CHOICES)
-    order_time = models.DateTimeField(_("Order Time"), default=timezone.now)
-    delivery_time = models.DateTimeField(_("Delivery Time"),null=True, blank=True)
+
+    def get_total(self):
+        total = 0
+        cart_details = self.cart_detail.all()
+        for product in cart_details:
+            total += product.total
+        return total
+
     def __str__(self):
         return self.code
+    
 
 
 class CartDetail(models.Model):
     order = models.ForeignKey(CartOrder, verbose_name=_("Order"), related_name='cart_detail', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, verbose_name=_("Product"), related_name='cart_product', on_delete=models.SET_NULL, null=True, blank=True)
-    quantity = models.FloatField(_("Quantity"),)
-    price = models.FloatField(_("Price"))
+    quantity = models.FloatField(_("Quantity"),default=0)
+    price = models.FloatField(_("Price"),default=0)
+    total = models.FloatField(_("Total"),default=0)
     def __str__(self):
         return str(self.order)
 
