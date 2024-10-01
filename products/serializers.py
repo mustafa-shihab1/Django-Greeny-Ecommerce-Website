2cae77c50976ad0934e772192982c430c22bed00
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-10-01 20:32:24 +0300

    serializer validation

--- products/serializers.py
+++ products/serializers.py
@@ -8,6 +8,11 @@ class ProductSerializer(serializers.ModelSerializer):
     def price_tax_calc(self, product:Product):
         return product.price * 1.1
 
+# this function will be executed in api.py when using -> serializer.is_valid(..)
+    # def validate(self, data):
+    #     if data['password1'] != data['password2']:
+    #         return serializers.ValidationError('RETURN TEXT ERROR!')
+
     class Meta:
         model = Product
         fields = ('__all__')
