ec5165ab150e67a2739006d50a5e2d11c59671b4
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-10-01 15:59:02 +0300

    add new field to API via serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    price_with_tax = serializers.SerializerMethodField(method_name='price_tax_calc')
+
+    def price_tax_calc(self, product:Product):
+        return product.price * 1.1
+
     class Meta:
         model = Product
         fields = ('__all__')
