2071a354e562ae0d5d0b3e7b24da20d2c2022f71
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-10-06 14:04:05 +0300

    simple JWT authentication

--- project/settings.py
+++ project/settings.py
@@ -67,7 +67,8 @@ REST_FRAMEWORK ={
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
 
--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -31,6 +35,10 @@ urlpatterns = [
     path('__debug__/',include('debug_toolbar.urls')),
     path("i18n/", include("django.conf.urls.i18n")),       # django translation
     path('rest-auth/', include('dj_rest_auth.urls')),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 
 ]
 
