a89b26c12965b7bdadbe949ad43e54b3a7218936
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-10-07 23:09:35 +0300

    edit model

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class ToDo(models.Model):
     title = models.CharField(max_length=100)
     done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
