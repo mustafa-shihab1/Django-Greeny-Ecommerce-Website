fd35d4dd1f11ab51453e244c60ae1152ced73252
mstfa_201 <mostafaa.sh2001@gmail.com>
2024-10-20 13:14:05 +0300

    edit products in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category, Brand, Review, ProductImages
 from django_summernote.admin import SummernoteModelAdmin
+from django.db.models.aggregates import Avg
 
 # Register your models here.
 
@@ -11,6 +12,15 @@ class ProductImagesInline(admin.TabularInline):
 class ProductAdmin(SummernoteModelAdmin):
     inlines = [ProductImagesInline]
     summernote_fields = '__all__'
+    list_per_page = 50
+    list_display = ['name','category','brand','review_count','rate_avg']
+
+    def review_count(self,obj):
+        return obj.product_review.count()
+    
+    def rate_avg(self,obj):
+        avg = obj.product_review.aggregate(myAvg=Avg('rate'))
+        return avg['myAvg']
 
 
 admin.site.register(Product, ProductAdmin)
